9c9f55b5730bd092620b587d07e1379a6d2f2140
Martin Mirchev <martogt@gmail.com>
2023-09-18 18:53:19 +0300

    Softuni Python
    Signed-off-by: Martin Mirchev <martogt@gmail.com>

--- Python_advanced/tuples_and_sets/count_same_values.py
+++ Python_advanced/tuples_and_sets/count_same_values.py
@@ -1,2 +1,8 @@
-1234
-s
+nums = tuple(float(x) for x in input().split())
+
+occurrences = {}
+
+for num in nums:
+    if num not in occurrences:
+        occurrences[num] = nums.count(num)
+        print(f'{num} - {nums.count(num)} times')
