f5984b720441b109f35bffb305e92f8959245cc1
Martin Mirchev <martogt@gmail.com>
2023-09-25 22:11:18 +0300

    Softuni Python
    Signed-off-by: Martin Mirchev <martogt@gmail.com>

---
+++ Python_advanced/multidimensional_lists/exercise/2x2_squares_in_matrix.py
@@ -0,0 +1,18 @@
+rows, cols = (int(i) for i in input().split())
+
+matrix = [[chars for chars in input().split()] for col in range(rows)]
+counter = 0
+
+for row_i in range(rows - 1):
+    for col_i in range(cols - 1):
+        current_el = matrix[row_i][col_i]
+        next_el = matrix[row_i][col_i + 1]
+        bottom_el = matrix[row_i + 1][col_i]
+        diagonal_el = matrix[row_i + 1][col_i + 1]
+
+        if current_el == next_el == bottom_el == diagonal_el:
+            counter += 1
+        else:
+            continue
+
+print(counter)
