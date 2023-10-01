311e4270d4daa7d08a5beaac37fac9fcce5bc7ad
Martin Mirchev <martogt@gmail.com>
2023-10-01 19:49:18 +0300

    Softuni Python
    Signed-off-by: Martin Mirchev <martogt@gmail.com>

--- Python_advanced/functions_advanced/lab/cheese_showcase.py
+++ Python_advanced/functions_advanced/lab/cheese_showcase.py
@@ -3,7 +3,7 @@ def sorting_cheeses(**kwargs):
     result = ""
 
     for name, quantities in sorted_dict:
-        result += f"{name} \n"
+        result += f"{name}\n"
         sorted_quantities = sorted(quantities, reverse=True)
         result += "\n".join([str(el) for el in sorted_quantities])
         result += "\n"
---
+++ Python_advanced/functions_advanced/lab/operate.py
@@ -0,0 +1,1 @@
+def operate(sign, *args):
