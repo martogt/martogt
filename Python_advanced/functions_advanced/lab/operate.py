b2c65486b74029268dbc7e2d17f11e346a94603f
Martin Mirchev <martogt@gmail.com>
2023-10-01 19:58:03 +0300

    Softuni Python
    Signed-off-by: Martin Mirchev <martogt@gmail.com>

--- Python_advanced/functions_advanced/lab/operate.py
+++ Python_advanced/functions_advanced/lab/operate.py
@@ -1,1 +1,16 @@
+from functools import reduce
+
+
 def operate(sign, *args):
+    if sign == "+":
+        return reduce(lambda a, b: a + b, args)
+    elif sign == "-":
+        return reduce(lambda a, b: a - b, args)
+    elif sign == "*":
+        return reduce(lambda a, b: a * b, args)
+    elif sign == "/":
+        return reduce(lambda a, b: a / b, args)
+
+
+print(operate("+", 1, 2, 3))
+print(operate("*", 3, 4))
