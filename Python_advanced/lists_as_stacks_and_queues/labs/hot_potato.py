297e598cde2c9d3473b7deb421e32fcd2a97b488
Martin Mirchev <martogt@gmail.com>
2023-10-20 20:18:53 +0300

    Softuni Python
    Signed-off-by: Martin Mirchev <martogt@gmail.com>

---
+++ Python_advanced/lists_as_stacks_and_queues/labs/hot_potato.py
@@ -0,0 +1,10 @@
+from collections import deque
+
+children = deque(input().split())
+n = int(input()) - 1
+
+while len(children) - 1:
+    children.rotate(-n)
+    print(f"Removed {children.popleft()}")
+
+print(f"Last is {children.popleft()}")
