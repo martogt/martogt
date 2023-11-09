9fc3553b1ec8c4b64ef07b2ac96c694c5f649c8a
Martin Mirchev <martogt@gmail.com>
2023-11-09 21:31:01 +0200

    Softuni Python
    Signed-off-by: Martin Mirchev <martogt@gmail.com>

---
+++ Python_OOP/classes_and_objects/exercise/pizza_delivery.py
@@ -0,0 +1,30 @@
+class PizzaDelivery:
+    def __init__(self, name: str, price: float, ingredients: dict):
+        self.name = name
+        self.price = price
+        self.ingredients = ingredients
+        self.ordered = False
+
+    def add_extra(self, ingredient: str, quantity: int, price_per_quantity: float):
+        if ingredient in self.ingredients:
+            self.ingredients[self.name] += quantity
+            self.price += (price_per_quantity * quantity)
+        else:
+            pass
+
+    def remove_ingredient(self, ingredient: str, quantity: int, price_per_quantity: float):
+        pass
+
+    def make_order(self):
+        pass
+
+
+margarita = PizzaDelivery('Margarita', 11, {'cheese': 2, 'tomatoes': 1})
+margarita.add_extra('mozzarella', 1, 0.5)
+margarita.add_extra('cheese', 1, 1)
+margarita.remove_ingredient('cheese', 1, 1)
+print(margarita.remove_ingredient('bacon', 1, 2.5))
+print(margarita.remove_ingredient('tomatoes', 2, 0.5))
+margarita.remove_ingredient('cheese', 2, 1)
+print(margarita.make_order())
+print(margarita.add_extra('cheese', 1, 1))
